99205274e9c25864059c4cc7fcb5312074a08254
polarcube1 <polarcube1@gmail.com>
2022-06-05 18:27:03 -0400

    🎇 Flash Effect Complete

--- BeruApp/Assets/Scripts/ButtonCommands/Button_InstantiateObject.cs
+++ BeruApp/Assets/Scripts/ButtonCommands/Button_InstantiateObject.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button_InstantiateObject
+public class Button_InstantiateObject : MonoBehaviour
 { 
-    public void CreateObject(GameObject gameObject)
+    public void CreateObjectAtZeroZero(GameObject gameObject)
     {
         Object.Instantiate(gameObject, Vector3.zero, Quaternion.identity);
     }
