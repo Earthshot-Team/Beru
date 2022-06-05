38ead2f3b1426e869de8c8c616a6c8ea7b11a36e
polarcube1 <polarcube1@gmail.com>
2022-06-05 16:57:18 -0400

    ❌ Backtracking Line Scaling
    Won't be adding the line scaling for now, not a priority.

--- BeruApp/Assets/Scripts/ScaleTopBarUI.cs
+++ BeruApp/Assets/Scripts/ScaleTopBarUI.cs
@@ -16,27 +16,10 @@ public class ScaleTopBarUI : MonoBehaviour
     [SerializeField] private float textBuffer = 30f;
     [SerializeField] private float textSizeY = 75f;
 
-    [Header("Images")]
-    [SerializeField] private Image leftLine;
-    private RectTransform leftLineRect;
-    [SerializeField] private Image rightLine;
-    private RectTransform rightLineRect;
-
-    private void Awake()
-    {
-        leftLineRect = leftLine.rectTransform;
-        rightLineRect = rightLine.rectTransform;
-    }
-
     private void Start()
     {
         textComponent.text = word;
         ReloadTextBoxSize();
-    }
-
-    private void Update()
-    {
-        //leftLineRect.sizeDelta = textTransform.sizeDelta;
     }
 
     public void ReloadTextBoxSize()
