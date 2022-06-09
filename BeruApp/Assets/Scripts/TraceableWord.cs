adcab569b14a8ea30929d19409112d4790a592d7
polarcube1 <polarcube1@gmail.com>
2022-06-08 21:26:53 -0400

    🌐 Turned Letters to Meshes

--- BeruApp/Assets/Scripts/TraceableWord.cs
+++ BeruApp/Assets/Scripts/TraceableWord.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -49,8 +50,16 @@ public class TraceableWord : MonoBehaviour
     {
         GameObject obj = new GameObject("LetterObject " + data.Character.ToString());
 
-        SpriteRenderer spriteRend = obj.AddComponent<SpriteRenderer>();
-        spriteRend.sprite = data.UntracedSprite;
+        /*SpriteRenderer spriteRend = obj.AddComponent<SpriteRenderer>();
+        spriteRend.sprite = data.UntracedSprite;*/
+
+        Mesh mesh = SpriteToMesh(data.UntracedSprite);
+        mesh.name = data.Character.ToString() + " Mesh";
+
+        MeshFilter meshFilter = obj.AddComponent<MeshFilter>();
+        meshFilter.mesh = mesh;
+
+        MeshRenderer meshRenderer = obj.AddComponent<MeshRenderer>();
 
         obj.transform.SetParent(this.transform);
         obj.transform.localPosition = pos;
@@ -66,5 +75,15 @@ public class TraceableWord : MonoBehaviour
         {
             item.transform.localPosition -= new Vector3((instantiatePos.x - bufferBtwLetters) / 2, 0, 0); 
         }
+    }
+
+    Mesh SpriteToMesh(Sprite sprite)
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = Array.ConvertAll(sprite.vertices, i => (Vector3)i);
+        mesh.uv = sprite.uv;
+        mesh.triangles = Array.ConvertAll(sprite.triangles, i => (int)i);
+
+        return mesh;
     }
 }
